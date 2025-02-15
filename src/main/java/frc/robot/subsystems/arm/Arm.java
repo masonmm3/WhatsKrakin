98bf98da4dccebd07cedfb3a6ef0ad2e7825877a
FRC6366RamRodz <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-02-15 13:34:13 -0500

    Added first Arm code.

---
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.Alert;
+
+public class Arm extends SubsystemBase {
+private final String name;
+private final ArmIO io;
+protected final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
+private final Alert disconnected;
+private final Timer stateTimer = new Timer();
+
+
+public Arm(String name, ArmIO io) {
+this.name = name;
+this.io = io;
+
+disconnected = new Alert(name + " motor Disconnected", Alert.AlertType.kWarning);
+stateTimer.start();
+}
+
+public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs(name, inputs);
+    disconnected.set(!inputs.connected);
+}
+}
