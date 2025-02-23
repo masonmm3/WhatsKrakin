e12d0849db3abf644e5c44275d0d441769c30134
Nick Dupre <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-02-23 12:03:32 -0500

    working on Arm.java

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -26,78 +26,83 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 
 public class Arm extends SubsystemBase {
 
-  private final String name;
-  private final ArmIO io;
-  protected final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
-  private final Alert disconnected;
-  protected final Timer stateTimer = new Timer();
+  //soft limits
+  public static final Rotation2d minAngle = Rotation2d.fromDegrees(0);
+  public static final Rotation2d maxAngle = Rotation2d.fromDegrees(7);
+  public static final Rotation2d minExtend = Rotation2d.fromRotations(0);
+  public static final Rotation2d maxExtend = Rotation2d.fromRotations(7);
 
-  public Arm(String name, ArmIO io) {
-    this.name = name;
-    this.io = io;
+  //Tunable numbers
+  private static final LoggedTunableNumber kPPivot = new LoggedTunableNumber("Arm/kP");
+  private static final LoggedTunableNumber kDPivot = new LoggedTunableNumber("Arm/kD");
+  private static final LoggedTunableNumber kSPivot = new LoggedTunableNumber("Arm/kS");
+  private static final LoggedTunableNumber kGPivot = new LoggedTunableNumber("Arm/kG");
+  private static final LoggedTunableNumber maxVelocityDegPerSecPivot = 
+  new LoggedTunableNumber("Arm/MaxVelocityDegreesPerSec", 500);
+  private static final LoggedTunableNumber maxAccelerationDegPerSec2Pivot = 
+  new LoggedTunableNumber("Arm/MaxAccelerationDegPerSec2", 2000);
+  private static final LoggedTunableNumber staticCharacterizationVelocityThreshPivot = 
+  new LoggedTunableNumber("Arm/StaticCharaterizationVelocityThresh", 0.1);
+  private static final LoggedTunableNumber kPExtend = new LoggedTunableNumber("Arm/kP");
+  private static final LoggedTunableNumber kDExtend = new LoggedTunableNumber("Arm/kD");
+  private static final LoggedTunableNumber kSExtend = new LoggedTunableNumber("Arm/kS");
+  private static final LoggedTunableNumber kGExtend = new LoggedTunableNumber("Arm/kG");
+  private static final LoggedTunableNumber maxVelocityDegPerSecExtend = 
+  new LoggedTunableNumber("Arm/MaxVelocityDegreesPerSec", 500);
+  private static final LoggedTunableNumber maxAccelerationDegPerSec2Extend = 
+  new LoggedTunableNumber("Arm/MaxAccelerationDegPerSec2", 2000);
+  private static final LoggedTunableNumber staticCharacterizationVelocityThreshExtend = 
+  new LoggedTunableNumber("Arm/StaticCharaterizationVelocityThresh", 0.1);
 
-    disconnected = new Alert(name + " motor disconnected!", Alert.AlertType.kWarning);
-    stateTimer.start(); 
-
-
-    public static final Rotation2d minAngle = Rotation2d.fromDegrees(0);
-    public static final Rotation2d maxAngle = Rotation2d.fromDegrees(7);
-    public static final Rotation2d minExtend = Rotation2d.fromRotations(0);
-    public static final Rotation2d maxExtend = Rotation2d.fromRotations(7);
-
-    
-
-    //Tunable numbers
-    private static final LoggedTunableNumber kPPivot = new LoggedTunableNumber("Arm/kP");
-    private static final LoggedTunableNumber kDPivot = new LoggedTunableNumber("Arm/kD");
-    private static final LoggedTunableNumber kSPivot = new LoggedTunableNumber("Arm/kS");
-    private static final LoggedTunableNumber kGPivot = new LoggedTunableNumber("Arm/kG");
-    private static final LoggedTunableNumber maxVelocityDegPerSecPivot = 
-    new LoggedTunableNumber("Arm/MaxVelocityDegreesPerSec", 500);
-    private static final LoggedTunableNumber maxAccelerationDegPerSec2Pivot = 
-    new LoggedTunableNumber("Arm/MaxAccelerationDegPerSec2", 2000);
-    private static final LoggedTunableNumber staticCharacterizationVelocityThreshPivot = 
-    new LoggedTunableNumber("Arm/StaticCharaterizationVelocityThresh", 0.1);
-    private static final LoggedTunableNumber kPExtend = new LoggedTunableNumber("Arm/kP");
-    private static final LoggedTunableNumber kDExtend = new LoggedTunableNumber("Arm/kD");
-    private static final LoggedTunableNumber kSExtend = new LoggedTunableNumber("Arm/kS");
-    private static final LoggedTunableNumber kGExtend = new LoggedTunableNumber("Arm/kG");
-    private static final LoggedTunableNumber maxVelocityDegPerSecExtend = 
-    new LoggedTunableNumber("Arm/MaxVelocityDegreesPerSec", 500);
-    private static final LoggedTunableNumber maxAccelerationDegPerSec2Extend = 
-    new LoggedTunableNumber("Arm/MaxAccelerationDegPerSec2", 2000);
-    private static final LoggedTunableNumber staticCharacterizationVelocityThreshExtend = 
-    new LoggedTunableNumber("Arm/StaticCharaterizationVelocityThresh", 0.1);
-  }
-
-
-
-
-    static {
-      switch (Constants.RobotType()) {
-        case SIMBOT -> {
-          kP.initDefault(4000);
-          kD.initDefault(1000);
-          kS.initDefault(1.2);
-          kG.initDefault(0.0);
-        }
-          
-        default -> {
-          kP.initDefault(0);
-          kD.initDefault(0);
-          kS.initDefault(0);
-          kG.initDefault(0);
-        }
-          
+  static {
+    switch (Constants.getRobot()) {
+      case SIMBOT -> {
+        kPPivot.initDefault(4000);
+        kDPivot.initDefault(1000);
+        kSPivot.initDefault(1.2);
+        kGPivot.initDefault(0.0);
+        kPExtend.initDefault(4000);
+        kDExtend.initDefault(1000);
+        kSExtend.initDefault(1.2);
+        kGExtend.initDefault(0.0);
+      }
+        
+      default -> {
+        kPPivot.initDefault(0);
+        kDPivot.initDefault(0);
+        kSPivot.initDefault(0);
+        kGPivot.initDefault(0);
+        kPExtend.initDefault(0);
+        kDExtend.initDefault(0);
+        kSExtend.initDefault(0);
+        kGExtend.initDefault(0);
       }
     }
+  }
+
+  //hardware
+  private final ArmIO io;
+  protected final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
+
+  //overrides
+  private BooleanSupplier coastOverride = () -> false;
+  private BooleanSupplier disabledOverride = () -> false;
+
+
+  private final Alert disconnected;
+
+  protected final Timer stateTimer = new Timer();
+
+
+
+
+    
 
     private final ArmIO armIO;
     private final ArmIOInputsAutoLogged armInputs = new ArmIOInputsAutoLogged();
     
 
-    private BooleanSupplier coastOverride = () -> false;
-    private BooleanSupplier disabledOverride = () -> false;
+    
     
     private TrapezoidProfile profile;
     
