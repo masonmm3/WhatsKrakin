a80fdf143fb625912ae26c5f3bd28cd1834a4ccd
FRC6366RamRodz <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-02-23 15:03:01 -0500

    Updated Arm.Java

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.arm;
 
 import static frc.robot.subsystems.arm.ArmConstants.*;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
@@ -18,6 +19,8 @@ import frc.robot.Constants;
 import frc.robot.util.LoggedTunableNumber;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
 import lombok.Getter;
 import frc.robot.Constants;
 import frc.robot.Constants.RobotType;
@@ -107,9 +110,9 @@ public class Arm extends SubsystemBase {
   @AutoLogOutput private boolean hasCoral = false;
 
   //  @Setter private double extendVolts = 0.0;
-  //  @Setter private double pivotAngle = 0.0;
+   // @Setter private double pivotAngle = 0.0;
 
-  // private Debouncer armDebouncer = new Debouncer(0.1);
+   private Debouncer armDebouncer = new Debouncer(0.1);
 
   // Disconnected alerts
   private final Alert pivotMotorDisconnectedAlert =
@@ -167,10 +170,126 @@ public class Arm extends SubsystemBase {
     setBrakeModePivot(!coastOverridePivot.getAsBoolean());
     setBrakeModeExtend(!coastOverridePivot.getAsBoolean());
 
+    //Run profile
+    final boolean shouldRunProfile = 
+    !stopProfile && !coastOverridePivot.getAsBoolean()
+      && !disabledOverridePivot.getAsBoolean() 
+      && !isEStopped
+      && DriverStation.isEnabled();
+      Logger.recordOutput("Dispenser/RunningProfile", shouldRunProfile);
+
+
+//check is out of tolerance 
+boolean outOfTolerance = Math.abs(getPivotAngle().getRadians() - setpoint.position)  > tolerance.get();   
+shouldEStop = toleranceDebouncer.calculate(outOfTolerance && shouldRunProfile)
+|| getPivotAngle().getRadians() < minAngle.getRadians() ||
+getPivotAngle().getRadians() > maxAngle.getRadians();
+
+if (shouldRunProfile) {
+  //clamp goal
+  var = goalState = 
+  new State(
+    MathUtil.clamp(goal.getAsDouble(), minAngle.getRadians, maxAngle.getRadians), 0.0);
+  setpoint = profile.calculate(Constants.loopPeriodSecs, setpoint, goalState);
+  armIO.runPosition(
+    Rotation2d.fromRadians(setpoint.position),
+    kSPivot.get() * Math.signum(setpoint.position),
+    kGPivot.get() * getPivotAngle().getCos());
+    //check at goal
+atGoal = 
+EqualsUtil.epsilonEquals(setpoint.position, goalState.position)
+&& EqualsUtil.epsilonEquals(setpoint.velocity, 0.0);
+
+//Log state
+Logger.recordOutput("Arm/Profile/SetpointPositionRad", setpoint.position);
+Logger.recordOutput("Arm/Profile/SetpointVelocityRadPerSec", setpoint.velocity);
+Logger.recordOutput("Arm/Profile/GoalPositionRad", goalState.position);
+} else {
+  //Reset setpoint
+  setpoint = new State(getPivotAngle().getRadians(), 0.0);
+
+  //Clear logs
+  Logger.recordOutput("Arm/Profile/SetpointPositionRad", 0.0);
+Logger.recordOutput("Arm/Profile/SetpointVelocityRadPerSec", 0.0);
+Logger.recordOutput("Arm/Profile/GoalPositionRad", 0.0);
+}
+// Log State
+Logger.recordOutput("Arm/CoastOverridePivot", coastOverridePivot.getAsBoolean());
+Logger.recordOutput("Arm/CoastOverrideExtend", coastOverrideExtend.getAsBoolean());
+Logger.recordOutput("Arm/DisabledOverrideExtend", disabledOverrideExtend.getAsBoolean());
+Logger.recordOutput("Arm/DisabledOverridePivot", disabledOverridePivot.getAsBoolean());
+
+//Record cycle time
+LoggedTracer.record("Arm");
+
   }
+
+  public void setGoal(Supplier<Rotation2d> goal) {
+    this.goal = () -> MathUtil.inputModulus(goal.get(), -3.0 * Math.PI 
+    /2.0, Math.PI / 2.0);
+    atGoal = false;
+  }
+
+  public double getGoal() {
+    return goal.getAsDouble();
+  }
+
+public Rotation2d getPivotAngle(){
+  return armInputs.data.internalPositionExtend();
+  return armInputs.data.internalPositionPivot();
+}
+
+public void setOverrides(BooleanSupplier coastOverridePivot, BooleanSupplier coastOverrideExtend, BooleanSupplier disabledOverridePivot, BooleanSupplier disabledOverrideExtend){
+  this.coastOverrideExtend = coastOverrideExtend;
+  this.coastOverridePivot = coastOverridePivot;
+  this.disabledOverrideExtend = disabledOverrideExtend;
+this.disabledOverridePivot = disabledOverridePivot;
+}
+
+private void setBrakeModeExtend(boolean enabled){
+  if (brakeModeEnabled == enabled) return;
+  brakeModeEnabled = enabled;
+  armIO.setBrakeModeExtend(enabled);
+}
+private void setBrakeModePivot(boolean enabled){
+  if (brakeModeEnabled == enabled) return;
+  brakeModeEnabled = enabled;
+  armIO.setBrakeModePivot(enabled);
+}
+
+
+public Command staticCharacterization(double outputRampRate) {
+  final staticCharacterizationState state = new staticCharacterizationState();
+  Timer timer = new Timer();
+  return Command.startRun(
+    () -> { 
+      staticCharacterizationOutput = outputRampRate * timer.get();
+      armIO.runOpenLoopExtend(state.CharacterizationOutput);
+      staticCharacterizationOutput = outputRampRate * timer.get();
+      armIO.runOpenLoopPivot(state.CharacterizationOutput);
+      Logger.recordOutput("Arm/staticCharacterizationOutputExtend");
+      Logger.recordOutput("Arm/staticCharacterizationOutputPivot");
+    }).until(() -> 
+    armInputs.data.velocityRadPErSec() >= 
+    staticCharacterizationVelocityThreshExtend.get(),
+    staticCharacterizationVelocityThreshPivot.get()
+    ).finallyDo(
+      () -> {
+        stopProfile = false;
+        timer.stop();
+        Logger.recordOutput("Arm/CharacterizationOutput", state.CharacterizationOutput);
+      }
+    );
+}
+
+private static class StaticCharacterizationState {
+  public double characterizationOutput = 0.0;
+}
+
 
   @AutoLogOutput
   public Command runArm(double inputVolts) {
     return startEnd(() -> io.runVolts(inputVolts), () -> io.stop());
   }
 }
+
