480503e0bf220fe6a00c23d5f3c4628857200b91
masonmm3 <53064865+masonmm3@users.noreply.github.com>
2025-02-28 11:11:14 -0500

    initial file setup

---
+++ src/main/java/frc/robot/subsystems/SuperStructure/Extension/Extension.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.SuperStructure.Extension;
+
+import org.littletonrobotics.junction.Logger;
+
+/** Add your docs here. */
+public class Extension {
+    private ExtensionIO io;
+    private ExtensionIOInputsAutoLogged inputs;
+
+    public Extension(ExtensionIO io) {
+        this.io = io;
+
+    }
+
+    public void armPeriodic() {
+        Logger.processInputs("Arm/Pivot", inputs);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/SuperStructure/Extension/ExtensionIO.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.SuperStructure.Extension;
+
+import org.littletonrobotics.junction.AutoLog;
+
+/** Add your docs here. */
+public interface ExtensionIO {
+
+  @AutoLog
+  public static class ExtensionIOInputs {}
+}
